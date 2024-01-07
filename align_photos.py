872b1716ff92400309aec7e9ab67524faf75a319
Darian Patzak <my-dev@outlook.de>
2024-01-07 01:28:52 +0100

    classify eyes as right and left

--- align_photos.py
+++ align_photos.py
@@ -1,20 +1,23 @@
+from PIL import Image
 import pathlib
 import cv2
 
 PIC_PATH = 'pictures'
+EXPORT_PATH = 'out'
 
 
 FACE_CASCADE_PATH = pathlib.Path(cv2.__file__).parent.absolute() / 'data/haarcascade_frontalface_default.xml'
 EYES_CASCADE_PATH = pathlib.Path(cv2.__file__).parent.absolute() / 'data/haarcascade_eye.xml'
 
-
 face_cascade = cv2.CascadeClassifier( str(FACE_CASCADE_PATH) )
 eyes_cascade = cv2.CascadeClassifier( str(EYES_CASCADE_PATH) )
 
 
+TARGET_EYE_LEFT: tuple[int, int]
+TARGET_EYE_RIGHT: tuple[int, int]
 
 
-for img_path in pathlib.Path(PIC_PATH).iterdir():
+for i, img_path in enumerate( pathlib.Path(PIC_PATH).iterdir() ):
 
     img = cv2.imread( str(img_path), 1)  # load image in grayscale
 
@@ -27,10 +30,11 @@ for img_path in pathlib.Path(PIC_PATH).iterdir():
         flags=cv2.CASCADE_SCALE_IMAGE
     )
 
+    # loop over biggest face, so only executed if found one at all
     for (x1, y1, width_face, height_face) in sorted(list(faces), key=lambda f:f[2])[:1]:
         # cv2.rectangle( img, (x1, y1), (x1+width_face, y1+height_face), (255, 255, 0), 6 )
 
-        # EYES
+        # find Eyes
         faceROI = img[y1:y1+height_face, x1:x1+width_face]
 
         eyes = eyes_cascade.detectMultiScale(
@@ -44,11 +48,34 @@ for img_path in pathlib.Path(PIC_PATH).iterdir():
         if len(list(eyes)) != 2:
             continue
 
-        for (x2, y2, width_eye, height_eye) in eyes:
+        source_eye_left: tuple[int, int]
+        source_eye_right: tuple[int, int]
+
+        # find both eyes, limit list to two eyes, don't know if they're ever more than two
+        for j, (x2, y2, width_eye, height_eye) in enumerate(eyes[:2]):
             eye_center = ( x1 + x2 + width_eye // 2, y1 + y2 + height_eye // 2 )
             radius = int(round( width_eye / 2 ))
 
+            # blindly assign eyes to variables and swap them if the right eye is more left than the left eye
+            if j == 0:
+                source_eye_left  = eye_center
+            else:
+                source_eye_right = eye_center
+
+                if source_eye_right[0] < source_eye_left[0]:
+                    source_eye_left, source_eye_right = source_eye_right, source_eye_left
+
             cv2.rectangle( img, (x1+x2, y1+y2), (x1+x2+width_eye, y1+y2+height_eye), (255, 255, 0), 6 )
+
+        # set global target eye coordinates
+        if i == 0:
+            TARGET_EYE_LEFT  = source_eye_left
+            TARGET_EYE_RIGHT = source_eye_right
+
+
+
+
+
 
 
 
